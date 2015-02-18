0a67425afccbfe54083ab680becb8fa50c6c91e3
Dave Kessler <activethrasher00@gmail.com>
2015-02-18 10:24:25 +0200

    Power Menu: remove power as an option
    This is the only way to power the device off. Also, if the user
    toggles all the options off an empty dialog will open.  Lets make
    this feature a little more user-friendly and remove power as an
    optional entry.

--- src/com/android/settings/cyanogenmod/PowerMenuFragment.java
+++ src/com/android/settings/cyanogenmod/PowerMenuFragment.java
@@ -37,7 +37,6 @@ import java.util.List;
 
 public class PowerMenuFragment extends PreferenceFragment {
 
-    private SwitchPreference mPowerPref;
     private SwitchPreference mRebootPref;
     private SwitchPreference mScreenshotPref;
     private SwitchPreference mAirplanePref;
@@ -72,9 +71,7 @@ public class PowerMenuFragment extends PreferenceFragment {
                 continue;
             }
 
-            if (action.equals(GLOBAL_ACTION_KEY_POWER)) {
-                mPowerPref = (SwitchPreference) findPreference(GLOBAL_ACTION_KEY_POWER);
-            } else if (action.equals(GLOBAL_ACTION_KEY_REBOOT)) {
+            if (action.equals(GLOBAL_ACTION_KEY_REBOOT)) {
                 mRebootPref = (SwitchPreference) findPreference(GLOBAL_ACTION_KEY_REBOOT);
             } else if (action.equals(GLOBAL_ACTION_KEY_SCREENSHOT)) {
                 mScreenshotPref = (SwitchPreference) findPreference(GLOBAL_ACTION_KEY_SCREENSHOT);
@@ -105,10 +102,6 @@ public class PowerMenuFragment extends PreferenceFragment {
     @Override
     public void onStart() {
         super.onStart();
-
-        if (mPowerPref != null) {
-            mPowerPref.setChecked(settingsArrayContains(GLOBAL_ACTION_KEY_POWER));
-        }
 
         if (mRebootPref != null) {
             mRebootPref.setChecked(settingsArrayContains(GLOBAL_ACTION_KEY_REBOOT));
@@ -172,11 +165,7 @@ public class PowerMenuFragment extends PreferenceFragment {
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
         boolean value;
 
-        if (preference == mPowerPref) {
-            value = mPowerPref.isChecked();
-            updateUserConfig(value, GLOBAL_ACTION_KEY_POWER);
-
-        } else if (preference == mRebootPref) {
+        if (preference == mRebootPref) {
             value = mRebootPref.isChecked();
             updateUserConfig(value, GLOBAL_ACTION_KEY_REBOOT);
 
