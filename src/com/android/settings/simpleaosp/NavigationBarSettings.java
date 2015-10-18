6724fbe470277c0c439daaaa04caae4b357fc485
snak3ater <saeedyasir91@gmail.com>
2015-10-18 15:35:24 +0100

    Settings: Add navigation bar fragment
    Change-Id: I4603582b713ce71f7f53299c8414d65c48869f4f

---
+++ src/com/android/settings/simpleaosp/NavigationBarSettings.java
@@ -0,0 +1,40 @@
+package com.android.settings.simpleaosp;
+
+import android.os.Bundle;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class NavigationBarSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+		// private variables here
+		
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.navigation_bar_settings);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+
+		// preference changes here
+        return false;
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.SAOSP_TWEAKS;
+    }
+}
+
