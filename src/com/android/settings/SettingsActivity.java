54970ebb4ef8424c394e54a05c7905adfa6ed445
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-12-01 13:54:45 +0000

    Settings: replace some toasts with a custom snackbar
    Replaced toasts
    * APN reset
    * Developer options unlock
    * Sim pin change
    * Reset network settings
    * Zen auto mode
    
    Change-Id: Iad906b69d142acf368cdc333b0bf989d098672f1

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -135,6 +135,7 @@ import com.android.settingslib.drawer.SettingsDrawerActivity;
 import com.android.settingslib.drawer.Tile;
 
 import com.android.settings.simpleaosp.AdditionalSettings;
+import com.nispok.snackbar.SnackbarManager;
 
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -844,6 +845,7 @@ public class SettingsActivity extends SettingsDrawerActivity
         unregisterReceiver(mBatteryInfoReceiver);
         unregisterReceiver(mUserAddRemoveReceiver);
         mDynamicIndexableContentMonitor.unregister();
+        SnackbarManager.dismiss();
     }
 
     @Override
