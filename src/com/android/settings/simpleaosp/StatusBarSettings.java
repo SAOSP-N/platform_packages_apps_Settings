f80ef51e73d64986195271a8fbb4aef57c95ae0b
BigBrother1984 <carlosavignano@aospa.co>
2016-03-26 16:02:12 -0400

    Improve Heads up toggle [2/2] * Add multiuser support, provide a system setting to override heads up   showing. User can disable heads up within this one so we don't need to   mess with Settings.Global heads up dependencies and break Google wide   implementations like priority filtering etc.
    PureNexus Edits:
    *Adapted for Marshmallow/Nougat

--- src/com/android/settings/simpleaosp/StatusBarSettings.java
+++ src/com/android/settings/simpleaosp/StatusBarSettings.java
@@ -30,10 +30,10 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
         mHeadsUp = (PreferenceScreen) findPreference(KEY_HEADS_UP_SETTINGS);
     }
 
-     private boolean getUserHeadsUpState() {
-         return Settings.Global.getInt(getContentResolver(),
-                Settings.Global.HEADS_UP_NOTIFICATIONS_ENABLED,
-                Settings.Global.HEADS_UP_ON) != 0;
+    private boolean getUserHeadsUpState() {
+         return Settings.System.getInt(getContentResolver(),
+                Settings.System.HEADS_UP_USER_ENABLED,
+                Settings.System.HEADS_UP_USER_ON) != 0;
     }
 
     @Override
