f338e4d1a01f55dc1fa5b142ada437ee3cf538f6
bigrushdog <randall.rushing@gmail.com>
2016-10-22 03:57:20 -0400

    Fling: Fix FC in Fling settings when configuration changes
    throws IllegalStateException when screen is rotated. Remove
    offending and unnecessary code.

--- src/com/android/settings/du/nav/FlingSettings.java
+++ src/com/android/settings/du/nav/FlingSettings.java
@@ -77,11 +77,6 @@ public class FlingSettings extends ActionFragment implements
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.fling_settings);
 
-        ActionBar bar = getActivity().getActionBar();
-        if (bar != null) {
-            bar.setTitle(R.string.fling_interface);
-        }
-
         mContext = (Context) getActivity();
         mIconPickHelper = new IconPickHelper(getActivity(), this);
 
@@ -349,3 +344,4 @@ public class FlingSettings extends ActionFragment implements
         return MetricsEvent.SAOSP_TWEAKS;
     }
 }
+
