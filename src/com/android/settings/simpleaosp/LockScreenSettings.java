42bb581f6a1449b8fe11c6719e0abe3d7434dde7
beanstown106 <nbenis106@gmail.com>
2016-11-27 03:29:59 -0500

    Fingerprint authentication vibration [2/2]

--- src/com/android/settings/simpleaosp/LockScreenSettings.java
+++ src/com/android/settings/simpleaosp/LockScreenSettings.java
@@ -1,10 +1,12 @@
 package com.android.settings.simpleaosp;
 
 import android.content.Context;
+import android.hardware.fingerprint.FingerprintManager;
 import android.os.Bundle;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.SwitchPreference;
 import android.provider.SearchIndexableResource;
@@ -26,14 +28,26 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener, Indexable {
 
     private static final String KEYGUARD_TORCH = "keyguard_toggle_torch";
+    private static final String FINGERPRINT_VIB = "fingerprint_success_vib";
+    private static final String LS_SECURE_CAT = "lockscreen_settings_category";
 
     private SystemSettingSwitchPreference mLsTorch;
+    private SystemSettingSwitchPreference mFingerprintVib;
+    private FingerprintManager mFingerprintManager;
 		
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.lock_screen_settings);
+
+        PreferenceCategory lockscreenCategory = (PreferenceCategory) findPreference(LS_SECURE_CAT);
+
+        mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
+        mFingerprintVib = (SystemSettingSwitchPreference) findPreference(FINGERPRINT_VIB);
+        if (!mFingerprintManager.isHardwareDetected()){
+            lockscreenCategory.removePreference(mFingerprintVib);
+        }
 
         mLsTorch = (SystemSettingSwitchPreference) findPreference(KEYGUARD_TORCH);
         if (!Utils.deviceSupportsFlashLight(getActivity())) {
