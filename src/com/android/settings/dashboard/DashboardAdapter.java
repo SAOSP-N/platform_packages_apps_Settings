nitrogen-project/android_packages_apps_Settings

Disable dashboard suggestions [1/2] and Improvements to dashboard suggestions by @snak3ater - Change the logic - Users might get confused so adding proper summaries for enable and disable suggestions
http://i.imgur.com/OJv71IN.png
http://i.imgur.com/ylojEgY.png

I replaced database keyname for new logic)

Conflicts:
	res/values-ru/nitrogen_strings.xml
	res/values/nitrogen_strings.xml

src/com/android/settings/dashboard/DashboardAdapter.java
@@ -42,6 +42,7 @@ import com.android.settings.dashboard.conditional.ConditionAdapterUtils;
 import com.android.settingslib.SuggestionParser;
 import com.android.settingslib.drawer.DashboardCategory;
 import com.android.settingslib.drawer.Tile;
+import android.provider.Settings;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -90,12 +91,23 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.Dash
     }
 
     public List<Tile> getSuggestions() {
-        return mSuggestions;
+        if ((Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.ENABLE_SUGGESTIONS, 1) == 1)) {
+             return mSuggestions;
+        } else {
+             return null;
+        }
     }
 
     public void setSuggestions(List<Tile> suggestions) {
-        mSuggestions = suggestions;
-        recountItems();
+        if ((Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.ENABLE_SUGGESTIONS, 1) == 1)) {
+             mSuggestions = suggestions;
+             recountItems();
+        } else {
+             mSuggestions = null;
+             recountItems();
+        }
     }
 
     public Tile getTile(ComponentName component) {

SAOSP-N/platform_packages_apps_Settings

src/com/android/settings/dashboard/DashboardAdapter.java
@@ -43,6 +43,7 @@ import com.android.settings.dashboard.conditional.ConditionAdapterUtils;
 import com.android.settingslib.SuggestionParser;
 import com.android.settingslib.drawer.DashboardCategory;
 import com.android.settingslib.drawer.Tile;
+import android.provider.Settings;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -106,13 +107,24 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.Dash
     }
 
     public List<Tile> getSuggestions() {
-        return mSuggestions;
+        if ((Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.DISABLE_SUGGESTIONS, 1) == 1)) {
+             return mSuggestions;
+        } else {
+             return null;
+        }
     }
 
     public void setCategoriesAndSuggestions(List<DashboardCategory> categories,
             List<Tile> suggestions) {
-        mSuggestions = suggestions;
-        mCategories = categories;
+        if ((Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.DISABLE_SUGGESTIONS, 1) == 1)) {
+             mSuggestions = suggestions;
+        } else {
+             mSuggestions = null;
+        }
+             mCategories = categories;
+             recountItems();
 
         // TODO: Better place for tinting?
         TypedValue tintColor = new TypedValue();